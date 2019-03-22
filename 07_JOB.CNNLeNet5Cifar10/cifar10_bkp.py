8ad232b74793e081775830ea257339c6b5a16432
Jasper Han <hanjp02@gmail.com>
2019-03-22 21:11:39 +0800

    Feature complete, accuracy is to optimize.

--- 07_JOB.CNNLeNet5Cifar10/cifar10_bkp.py
+++ 07_JOB.CNNLeNet5Cifar10/cifar10_bkp.py
@@ -3,14 +3,15 @@
 #This file is the sample code of lecture mnist_lenet5_backward.py
 
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 import os
-import mnist_fwp as fwp
 import numpy as np
+
+import cifar10_fwp as fwp
+import cifar10_gends as gends
 
 BATCH_SIZE  = 200 
 TOTAL_STEPS = 50000
-
+TOTAL_NUM_EXAMPLES = 50000
 
 LR_BASE = 0.1
 LR_DECAY = 0.99
@@ -28,14 +29,11 @@ def bk_propagation(mnist):
 	x  = tf.placeholder(tf.float32, [BATCH_SIZE, fwp.IMAGE_XY_RES, fwp.IMAGE_XY_RES, fwp.IMAGE_CHANS])
 	y_ = tf.placeholder(tf.float32, shape = (None, fwp.OUTPUT_NODE))
 
-	#X, Y_, Y_c = gends.generateds()
 
 	y = fwp.fw_propagation(x, True, REGULARIZER)
 
 	global_step = tf.Variable(0, trainable = False)
 
-	#loss_mse = tf.reduce_mean(tf.square(y - y_))
-	#loss = loss_mse
 
 	ce = tf.nn.sparse_softmax_cross_entropy_with_logits(logits = y, labels = tf.argmax(y_, 1))
 	loss_cem = tf.reduce_mean(ce)
@@ -46,7 +44,7 @@ def bk_propagation(mnist):
 	learning_rate = tf.train.exponential_decay(
 		LR_BASE,
 		global_step,
-		mnist.train.num_examples / BATCH_SIZE,
+		TOTAL_NUM_EXAMPLES / BATCH_SIZE,
 		LR_DECAY,
 		staircase = True)
 
@@ -61,15 +59,20 @@ def bk_propagation(mnist):
 		train_op = tf.no_op(name = "train")
 
 	saver = tf.train.Saver()
+	img_batch, label_batch = gends.get_tfRecords(BATCH_SIZE)
 
 	with tf.Session() as fcSess:
 		init_op = tf.global_variables_initializer()
 		fcSess.run(init_op)
 
+		#multi-threads start
+		coord = tf.train.Coordinator()
+		threads = tf.train.start_queue_runners(sess=fcSess, coord=coord)
+
 		ckpt = tf.train.get_checkpoint_state(MODEL_SAVE_PATH)
 		if ckpt and ckpt.model_checkpoint_path:
 			saver.restore(fcSess, ckpt.model_checkpoint_path)
-			xs, ys = mnist.train.next_batch(BATCH_SIZE)
+			xs, ys = fcSess.run([img_batch, label_batch])	
 			xs = np.reshape(xs, (BATCH_SIZE, fwp.IMAGE_XY_RES, fwp.IMAGE_XY_RES, fwp.IMAGE_CHANS))
 			_, loss_v, step = fcSess.run([train_op, loss, global_step], feed_dict = {x: xs, y_: ys})
 			print "Restored session at %d steps, loss is %g." %(step, loss_v)
@@ -82,16 +85,19 @@ def bk_propagation(mnist):
 				print "After %d steps, loss is %g." %(step, loss_v)
 				saver.save(fcSess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step = global_step)
 
-			xs, ys = mnist.train.next_batch(BATCH_SIZE)
+			xs, ys = fcSess.run([img_batch, label_batch])
 			xs = np.reshape(xs, (BATCH_SIZE, fwp.IMAGE_XY_RES, fwp.IMAGE_XY_RES, fwp.IMAGE_CHANS))
 			_, loss_v, step = fcSess.run([train_op, loss, global_step], feed_dict = {x: xs, y_: ys})
 		print "After %d steps, loss is %g." %(step, loss_v)
 		saver.save(fcSess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step = global_step)
 
+		#multi-threads end
+		coord.request_stop()
+		coord.join(threads)
+
 
 def main():
-	mnist = input_data.read_data_sets(DATA_SAVE_PATH, one_hot = True)
-	bk_propagation(mnist)
+	bk_propagation(None)
 
 if __name__ == "__main__":
 	main()
