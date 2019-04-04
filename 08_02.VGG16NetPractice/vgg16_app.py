87f3b4939151c724610baa03bc58fec5710d25ce
Jasper Han <hanjp02@gmail.com>
2019-04-04 16:43:12 +0800

    Coding completed, to optimize

--- 08_02.VGG16NetPractice/vgg16_app.py
+++ 08_02.VGG16NetPractice/vgg16_app.py
@@ -3,6 +3,55 @@
 #This file is the sample code of lecture app.py
 #Application file: read the file and implement recongnition.
 
-import tensorflow as tf
 import numpy as np
-from PIL import Image
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import vgg16_net as vgg16
+import vgg16_utils as utils
+from vgg16_nclasses import labels
+
+
+IMAGE_XY_RES = 224 
+IMAGE_CHANS  = 3
+
+def application():
+	#img_path  = raw_input("Please inut the image path:")
+	#img_ready = utils.load_image(img_path)
+	img_ready = utils.load_image("./pic/a.jpg")
+
+	fig = plt.figure(u"Top-5 预测结果")
+
+	with tf.Session() as sess:
+		images = tf.placeholder(tf.float32, [1, IMAGE_XY_RES, IMAGE_XY_RES, IMAGE_CHANS])
+		vgg = vgg16.Vgg16("./model/vgg16.npy")
+		vgg.fw_propagation(images)
+		probability = sess.run(vgg.prob, feed_dict={images:img_ready})
+		top5 = np.argsort(probability[0])[-1:-6:-1]
+		print "Top 5:", top5
+
+		values = []
+		bar_label = []
+		for n, i in enumerate(top5):
+			print "n:", n
+			print "i:", i
+			values.append(probability[0][i])
+			bar_label.append(labels[i])
+			print i, ":", labels[i], "----", utils.percent(probability[0][i])
+
+		ax = fig.add_subplot(111)
+		ax.bar(range(len(values)), values, tick_label=bar_label, width=0.5, fc='g')
+		ax.set_ylabel(u"probability")
+		ax.set_title(u"Top-5")
+
+		for a, b in zip(range(len(values)), values):
+			ax.text(a, b+0.005, utils.percent(b), ha='center', va='bottom', fontsize=7)
+
+		plt.show()
+
+
+def main():
+	application()
+
+if __name__ == '__main__':
+	main()
--- 08_02.VGG16NetPractice/vgg16_utils.py
+++ 08_02.VGG16NetPractice/vgg16_utils.py
@@ -3,6 +3,49 @@
 #This file is the sample code of lecture utils.py
 #Read the input picture, show the probability diagram.
 
-import tensorflow as tf
+from skimage import io, transform
 import numpy as np
-from PIL import Image
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from pylab import mpl
+
+import vgg16_app as app
+
+
+#正常显示中文标签
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+#正常显示正负号
+mpl.rcParams['axes.unicode_minus'] = False
+
+def load_image(path):
+	fig = plt.figure("Centre and Resize:")
+	img = io.imread(path)
+	img = img / 255.0
+
+	ax0 = fig.add_subplot(131)
+	ax0.set_xlabel(u'Original Picture')
+	ax0.imshow(img)
+
+	short_edge = min(img.shape[:2])
+	y = (img.shape[0] - short_edge ) / 2
+	x = (img.shape[1] - short_edge ) / 2
+	crop_img = img[y:y+short_edge, x:x+short_edge]
+
+
+	ax1 = fig.add_subplot(132)
+	ax1.set_xlabel(u'Centre Picture')
+	ax1.imshow(crop_img)
+
+	re_img = transform.resize(crop_img, (app.IMAGE_XY_RES, app.IMAGE_XY_RES))
+
+
+	ax2 = fig.add_subplot(133)
+	ax2.set_xlabel(u'Resize Picture')
+	ax2.imshow(re_img)
+
+	img_ready = re_img.reshape(1, app.IMAGE_XY_RES, app.IMAGE_XY_RES, app.IMAGE_CHANS)
+
+	return img_ready
+
+def percent(value):
+	return "%.2f%%" %(value * 100)
